b73655846f43f489bb61cf9bd5e8f3387bb321b5
Rave Flores <03100242.rave.f@031A-WEANB091.local>
2025-04-07 18:40:46 +0800

    automation version 1

---
+++ sys/homepage.js
@@ -0,0 +1,23 @@
+const automationDropdown = document.getElementById('automation-dropdown')
+automationDropdown.addEventListener('change', () => {
+
+    const selection = automationDropdown.value;
+
+    switch(selection){
+        case 'index.html':
+            window.location.href = selection;
+            break;
+        case 'newsletter.html': 
+            window.location.href = selection;
+            break;
+        case 'floating-banner.html': 
+            window.location.href = selection;
+            break;
+        case 'landing-page.html': 
+            window.location.href = selection;
+            break;
+        case 'widgets.html': 
+            window.location.href = selection;
+            break;
+    }
+})
---
+++ sys/updates.js
@@ -0,0 +1,14 @@
+document.getElementById('whats-new').addEventListener('click', () => {
+    document.getElementById('update-card').classList.toggle('hidden')
+})
+
+const update = {
+    text: `
+            <p class="p-2 border border-[#ff9200] text-xs rounded-sm text-white">Made changes to the UI for easier navigation.</p>
+            <p class="p-2 border border-[#ff9200] text-xs rounded-sm text-white">Added new automation: Floating Banner Automation</p>
+            <p class="p-2 border border-[#ff9200] text-xs rounded-sm text-white">Added new automation: Landing Page/Qualify Games Automation</p>
+            <p class="p-2 border border-[#ff9200] text-xs rounded-sm text-white">Added Live Casino/Casino icons to TNC Automation. Users now have the ability to add icons on their TNCs.</p>
+            <p class="p-2 border border-[#ff9200] text-xs rounded-sm text-white">Added widgets advert automation.</p>
+            `
+}
+const updateCard = document.getElementById('update-card').innerHTML = update.text;

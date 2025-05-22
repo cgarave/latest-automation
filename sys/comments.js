0ac5a086ae3842d2912e39dcbf78298d757cedb7
Rave Flores <03100242.rave.f@031A-WEANB091.local>
2025-05-22 13:27:34 +0800

    052225 added game list for both english and localized content

---
+++ sys/comments.js
@@ -0,0 +1,40 @@
+// live
+// <SCard class="my-4 bg-secondary">
+//   <SSectionHeading dark divider contained title-tag="h4"><span class="text-subtitle-1">Recommended Games</span>
+//   </SSectionHeading>
+//   <SList>
+//     <SListItem dark class="md:hover:bg-secondary--darken-4"><template #prependAvatar>
+//         <SAvatar src="https://doc.188contents.com/star4-content/images/live/game-provider/game-image-4x3-sm.webp" alt="game-image" size="40"
+//           class="!rounded-lg ml-6" />
+//       </template>game-name<template #appendAction>
+//         <GameLauncher v-slot="{openGame}" product="live" game="game-code">
+//           <SButton @click="openGame()" v-if="breakpoints.smAndUp" color="text-light--high"
+//             class="bg-primary--darken-5 hover:text-light--high mr-6">game-cta</SButton>
+//           <SButton @click="openGame()" v-else dark icon-only flat rounded class="mr-6">
+//             <SIcon>icon-arrow-right</SIcon>
+//           </SButton>
+//         </GameLauncher>
+//       </template></SListItem>
+//   </SList>
+// </SCard>
+
+
+// casino
+// <SCard class="my-4 bg-secondary">
+//   <SSectionHeading dark divider contained title-tag="h4"><span class="text-subtitle-1">Recommended Games</span>
+//   </SSectionHeading>
+//   <SList>
+//     <SListItem dark class="md:hover:bg-secondary--darken-4"><template #prependAvatar>
+//         <SAvatar src="https://doc.188contents.com/star4-content/images/live/game-provider/game-image/game-image-4x3-sm.webp" alt="game-image" size="40"
+//           class="!rounded-lg ml-6" />
+//       </template>game-name<template #appendAction>
+//         <GameLauncher v-slot="{openGame}" product="casino" game="game-code">
+//           <SButton @click="openGame()" v-if="breakpoints.smAndUp" color="text-light--high"
+//             class="bg-primary--darken-5 hover:text-light--high mr-6">game-cta</SButton>
+//           <SButton @click="openGame()" v-else dark icon-only flat rounded class="mr-6">
+//             <SIcon>icon-arrow-right</SIcon>
+//           </SButton>
+//         </GameLauncher>
+//       </template></SListItem>
+//   </SList>
+// </SCard>
